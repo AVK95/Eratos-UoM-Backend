a6cca356805f999085adc7af3ada618c28e53a9b
ENWENTSAI <44834280+ENWENTSAI@users.noreply.github.com>
2021-04-22 14:52:02 +0800

    Add files via upload

--- UoM-Server/Program.cs
+++ UoM-Server/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Net;
 
 namespace UoM_Server
 {
@@ -12,11 +14,41 @@ namespace UoM_Server
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main()
+        //static void Main()
+        //{
+        //    Application.EnableVisualStyles();
+        //    Application.SetCompatibleTextRenderingDefault(false);
+        //    Application.Run(new ServerGUI());
+        //}
+        static void Main(String[] args)
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new ServerGUI());
+           
+            Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            
+            IPAddress ipAddress = IPAddress.Parse("127.0.0.1");
+            IPEndPoint iPEndPoint = new IPEndPoint(ipAddress, 8888);
+            serverSocket.Bind(iPEndPoint);
+            serverSocket.Listen(0); // unlimited
+            Socket clientSocket = serverSocket.Accept(); //receive client connection
+
+            //server send msg to client
+            String msg = "hello client! ";
+            byte[] data = System.Text.Encoding.UTF8.GetBytes(msg);
+            clientSocket.Send(data);
+
+            //receive the msg
+            byte[] buffer = new byte[1024];
+            int count = clientSocket.Receive(buffer);
+
+            string Receivemsg = System.Text.Encoding.UTF8.GetString(buffer,0, count);
+            Console.WriteLine(Receivemsg);
+
+            Console.ReadKey();
+
+            clientSocket.Close();
+            serverSocket.Close();
+
         }
     }
 }
