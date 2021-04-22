d43bc02ff145b6a4696ef84745185721e104b998
ENWENTSAI <44834280+ENWENTSAI@users.noreply.github.com>
2021-04-22 14:51:09 +0800

    Add files via upload

--- UoM-Server/UoM-Client/Client.cs
+++ UoM-Server/UoM-Client/Client.cs
@@ -1,10 +1,31 @@
 ﻿using System;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
 namespace UoMClient
 {
-    public class Client
+    public class Program
     {
-        public Client()
+        static void Main(String[] args)
         {
+            Socket clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            clientSocket.Connect(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8888));
+
+            byte[] data = new byte[1024];
+            int count = clientSocket.Receive(data);
+            string msg = Encoding.UTF8.GetString(data, 0, count);
+
+            Console.Write(msg);
+
+            string s = Console.ReadLine();
+            Console.Write(s);
+
+            clientSocket.Send(Encoding.UTF8.GetBytes(s));
+
+
+            Console.ReadKey();
+            clientSocket.Close();
         }
     }
 }
