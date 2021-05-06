b9fb1e8271015e8d4d72f6f1934aa612ccc23bf6
deyouz <69853927+deyouz@users.noreply.github.com>
2021-05-06 19:51:28 +1000

    Add files via upload

--- UoM-Server/Program.cs
+++ UoM-Server/Program.cs
@@ -12,11 +12,17 @@ namespace UoM_Server
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main()
+        static async Task Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new ServerGUI());
+            //Console.WriteLine(new RequestHandler().CreateNewResource("https://schemas.eratos.ai/json/utas.climateinfo.csv", "average"));
+            //Console.WriteLine(new RequestHandler().ShowResourceVersions(""));
+            //Console.WriteLine(new RequestHandler().DeleteResource(""));
+            //Console.WriteLine(new RequestHandler().FetchGeometry("uxuq4pgc54ylxrklc4gz25fd", "wkt","max"));
+
+            //Console.WriteLine(RequestHandler.AUTHORISE_USER(Config.Access_Token));
         }
     }
 }
---
+++ UoM-Server/RequestHandler.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Web;
+using RestSharp;
+using RestSharp.Serializers.NewtonsoftJson;
+using System.IO;
+using System.IO.Compression;
+
+namespace UoM_Server
+{
+    class RequestHandler
+    {
+        string usingToken = Config.Access_Token;
+
+
+        public string CreateNewResource(string type, string name)
+        {
+
+            var client = new RestClient(Config.Primary_Node_Domain);
+            client.AddDefaultHeader("Authorization", string.Format("Bearer {0}", usingToken));
+
+            var payload = string.Format("\"@type\":\"{0}\", \"name\":\"{1}\"", type, name);
+            payload = "{" + payload + "}";
+            var request = new RestRequest("resources", Method.POST, DataFormat.Json);
+            request.AddJsonBody(payload);
+
+            var response = client.Execute(request);
+            return response.Content;
+        }
+
+
+        public string ShowResourceVersions(string resourceID)
+        {
+            var client = new RestClient(Config.Primary_Node_Domain);
+            client.AddDefaultHeader("Authorization", string.Format("Bearer {0}", usingToken));
+
+            var request = new RestRequest("resources/" + resourceID + "/versions", Method.GET, DataFormat.None);
+
+            var response = client.Execute(request);
+            return response.Content;
+        }
+
+        public string DeleteResource(string resourceID)
+        {
+            var client = new RestClient(Config.Primary_Node_Domain);
+            client.AddDefaultHeader("Authorization", string.Format("Bearer {0}", usingToken));
+
+            var request = new RestRequest("resources/" + resourceID, Method.DELETE, DataFormat.None);
+
+            var response = client.Execute(request);
+            return response.Content;
+        }
+
+
+        public string FetchGeometry(string resourceID, string type = null, string detail = null)
+        {
+            var client = new RestClient(Config.Primary_Node_Domain);
+            client.AddDefaultHeader("Authorization", string.Format("Bearer {0}", usingToken));
+
+            var requestPara = "";
+            if(type != null)
+            {
+                requestPara += "?type=" + type;
+            }
+
+            if(detail != null)
+            {
+                if(requestPara == "")
+                {
+                    requestPara += "?detail=" + detail;
+                }
+                else
+                {
+                    requestPara += "&detail=" + detail;
+                }
+            }
+
+            var request = new RestRequest("resources/" + resourceID + "/geo" + requestPara, Method.GET, DataFormat.None);
+
+            var response = client.Execute(request);
+            return response.Content;
+        }
+
+        
+        public static string AUTHORISE_USER(string token)
+        {
+            var client = new RestClient(Config.Tracker_Node_Domain);
+            client.AddDefaultHeader("Authorization", string.Format("Bearer {0}", token));
+
+            var request = new RestRequest("auth/me", Method.GET, DataFormat.None);
+
+            var response = client.Execute(request);
+            return response.Content;
+        }
+
+
+    }
+}
