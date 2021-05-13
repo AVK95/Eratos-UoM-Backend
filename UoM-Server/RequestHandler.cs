6c40f59dbf365af445399a2c1633dd1698c66249
Kelvin Wijaya <kwijaya1@student.unimelb.edu.au>
2021-05-13 12:39:23 +0700

    integrated paul's code

--- UoM-Server/RequestHandler.cs
+++ UoM-Server/RequestHandler.cs
@@ -48,7 +48,14 @@ namespace UoM_Server
         public string error { get; set; }
         public GNTaskMeta meta { get; set; }
     }
+        [DataContract]
+    public class PersonResource : Resource
+    {
+        [DataMember(Name = "description", EmitDefaultValue=false)]
+        public string description { get; set; }
 
+        
+    }
     [DataContract]
     public class Resource
     {
@@ -85,23 +92,45 @@ namespace UoM_Server
         [DataMember(Name = "name", EmitDefaultValue=false)]
         public string name { get; set; }
     }
-    public class PolicyResponse
+ [DataContract]
+    public class ResourcePolicyRule
     {
-        [JsonPropertyName("@id")]
+        [DataMember(Name = "actor")]
+        public string actor { get; set; }
+
+        [DataMember(Name = "effect")]
+        public string effect { get; set; }
+
+        [DataMember(Name = "actions")]
+        public List<string> actions { get; set; }
+    }
+    
+    [DataContract]
+    public class ResourcePolicy
+    {
+        [DataMember(Name = "@id", EmitDefaultValue=false)]
         public string id { get; set; }
-        [JsonPropertyName("@date")]
-        public string date { get; set; }
-        [JsonPropertyName("@owner")]
+
+        [DataMember(Name = "@owner", EmitDefaultValue=false)]
         public string owner { get; set; }
-        [JsonPropertyName("@for")] 
-        public string forAt { get; set; } 
-        [JsonPropertyName("@type")]
+
+        [DataMember(Name = "@for", EmitDefaultValue=false)]
+        public string resource { get; set; }
+
+        [DataMember(Name = "@type")]
         public string type { get; set; }
-        [JsonPropertyName("@invites")]
-        public PolicyInvites invites { get; set; }
-        [JsonPropertyName("@links")]
-        public PolicyProperties[] links { get; set; }
-        public PolicyRules rules { get; set; }
+
+        [DataMember(Name = "@date", EmitDefaultValue=false)]
+        public string date { get; set; }
+
+        [DataMember(Name = "@invites", EmitDefaultValue=false)]
+        public List<object> invites { get; set; }
+
+        [DataMember(Name = "@links", EmitDefaultValue=false)]
+        public Dictionary<string, object> links { get; set; }
+
+        [DataMember(Name = "rules")]
+        public List<ResourcePolicyRule> rules { get; set; }
     }
 
     public class PolicyInvites
@@ -142,7 +171,14 @@ namespace UoM_Server
             Normal,
             High
         }
-
+        static string SHA256HashBytesHex(byte[] content) {
+            byte[] hbytes;
+            using (SHA256 mySHA256 = SHA256.Create())
+            {
+                hbytes = mySHA256.ComputeHash(content);
+            }
+            return BitConverter.ToString(hbytes).Replace("-", "").ToLower();
+        }
         static string SHA256HashStringHex(string content)
         {
             byte[] hbytes;
@@ -170,7 +206,87 @@ namespace UoM_Server
             }
             return BitConverter.ToString(hbytes).Replace("-", "").ToLower();
         }
+        static string EratosSignedRequest(string accessId, string accessSecret, string method, string uri, string reqJSON)
+        {
+            byte[] body = Encoding.UTF8.GetBytes(reqJSON);
+            string curTime = DateTime.UtcNow.ToString("o");
+            string canonReq = method + "\n";
 
+            Uri uriObj = new Uri(uri);
+            canonReq += uriObj.LocalPath+"\n";
+            canonReq += uriObj.Query+"\n";
+
+            canonReq += "host:"+uriObj.Host+"\n";
+            canonReq += "x-eratos-date:"+curTime+"\n";
+            if (body.Length > 0) {
+                canonReq += "content-type:application/json\n";
+            }
+
+            string bodyHash = SHA256HashBytesHex(body);
+            canonReq += bodyHash;
+
+            string signedMAC = HMACHashStringHex(canonReq, accessSecret);
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);
+            request.Method = method;
+            if (body.Length > 0) {
+                request.ContentLength = body.Length;
+                request.ContentType = "application/json";
+            }
+            request.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
+            request.Headers.Set("X-Eratos-Date", curTime);
+            string signedHeaders = "host;x-eratos-date";
+            if (body.Length > 0) {
+                signedHeaders += ";content-type";
+            }
+            request.Headers.Set("Authorization", "ERATOS-HMAC-SHA256 Credential="+accessId+", SignedHeaders="+signedHeaders+", Signature="+signedMAC);
+
+            if (body.Length > 0) {
+                Stream dataStream = request.GetRequestStream();
+                dataStream.Write(body, 0, body.Length);
+                dataStream.Close();
+            }
+
+            try {
+                using(HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+                using(Stream stream = response.GetResponseStream())
+                using(StreamReader reader = new StreamReader(stream))
+                {
+                    return reader.ReadToEnd();
+                }
+            }
+            catch (WebException e)
+            {
+                using (WebResponse response = e.Response)
+                {
+                    HttpWebResponse httpResponse = (HttpWebResponse) response;
+                    Console.WriteLine("Error code: {0}", httpResponse.StatusCode);
+                    using (Stream data = response.GetResponseStream())
+                    using (var reader = new StreamReader(data))
+                    {
+                        string text = reader.ReadToEnd();
+                        Console.WriteLine(text);
+                        throw e;
+                    }
+                }
+            }
+        }
+        static string WriteObjToJSON<T>(T obj) {
+            var ms = new MemoryStream();
+            var ser = new DataContractJsonSerializer(typeof(T));
+            ser.WriteObject(ms, obj);
+            byte[] json = ms.ToArray();
+            ms.Close();
+            return Encoding.UTF8.GetString(json, 0, json.Length);
+        }
+        static T ReadObjFromJSON<T>(string json) where T : class, new() {
+            var dObj = new T();
+            var ms = new MemoryStream(Encoding.UTF8.GetBytes(json));
+            var ser = new DataContractJsonSerializer(dObj.GetType());
+            dObj = ser.ReadObject(ms) as T;
+            ms.Close();
+            return dObj;
+        }
         static string GetToken(string accessId, string accessSecret, string uri)
         {
             string curTime = DateTime.UtcNow.ToString("o");
@@ -361,6 +477,41 @@ namespace UoM_Server
 
             var response = client.Execute(request);
             return response.Content;
+        }
+
+        public string CreatePersonResource(string userName, string description)
+        {
+            PersonResource ps = new PersonResource();
+            ps.type = "https://schemas.eratos.ai/json/person";
+            ps.name = userName;
+            ps.description = description;
+
+            string psReq = WriteObjToJSON<PersonResource>(ps);
+            string json = EratosSignedRequest(accessId, accessSecret, "POST", "https://staging.e-pn.io/resources", psReq);
+            return json;
+
+        }
+
+        public ResourcePolicy GetPolicy(string personResource)
+        {
+            ps = ReadObjFromJSON<PersonResource>(personResource);
+            json = EratosSignedRequest(accessId, accessSecret, "GET", ps.policy, "");
+            ResourcePolicy pol = ReadObjFromJSON<ResourcePolicy>(json);
+            return pol;
+        }
+
+        public string AddUserToPolicy(ResourcePolicy pol, string userURI)
+        {
+            ResourcePolicyRule newRule = new ResourcePolicyRule();
+            newRule.actor = userURI; 
+            newRule.effect = "Allow";
+            newRule.actions = new List<string>() {
+                "Get", "Download"
+            };
+            pol.rules.Add(newRule);
+            string polReq = WriteObjToJSON<ResourcePolicy>(pol);
+            string response = EratosSignedRequest(accessId, accessSecret, "PUT", pol.id, polReq);
+            return response;
         }
 
         static Resource GetResource(string token, string id)
