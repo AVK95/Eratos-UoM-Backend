8f58759e19705f05c89c2d858866e2401e7fd103
Kelvin Wijaya <kwijaya1@student.unimelb.edu.au>
2021-05-19 14:19:23 +0700

    Revert "Revert "Merge branch 'main' of https://github.com/AVK95/Eratos-UoM-Backend into main""
    This reverts commit 60d48bb5728193c3d2aea18f7c08907a3ee28bcb.

---
+++ UoM-Server/Models/Order.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UoM_Server.Models
+{
+    public struct OrderTable
+    {
+        public int OrderID { get; set; }
+        public float Price { get; set; }
+        public string Status { get; set; }
+        public DateTime OrderTime { get; set; }
+        public int UserID { get; set; }
+
+        public OrderTable(int orderID, float price, string status, DateTime orderTime, int userID)
+        {
+            OrderID = orderID;
+            Price = price;
+            Status = status;
+            OrderTime = orderTime;
+            UserID = userID;
+        }
+    }
+}
---
+++ UoM-Server/Models/User.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using System.Text.Json;
+
+namespace UoM_Server.Models
+{
+    [DataContract]
+    public class User
+    {
+        [DataMember (Name = "auth0Id", EmitDefaultValue = false)]
+        public string auth0Id { get; set; }
+
+        [DataMember (Name = "id", EmitDefaultValue = false)]
+        public string id { get; set; }
+
+        [DataMember (Name = "info", EmitDefaultValue = false)]
+        public string info { get; set; }
+
+        [DataMember (Name = "resourcePolicy", EmitDefaultValue = false)]
+        public string resourcePolicy { get; set; }
+
+        [DataMember (Name = "termsLastAcceptedDate", EmitDefaultValue = false)]
+        public string termsLastAcceptedDate { get; set; }
+
+        [DataMember (Name = "termsLastAcceptedVersion", EmitDefaultValue = false)]
+        public int termsLastAcceptedVersion { get; set; }
+    }
+
+    public struct UserTable
+    {
+        public int UserID { get; set; }
+        public string EratosUserID { get; set; }
+        public string Email { get; set; }
+        public string Name { get; set; }
+        public string Auth0ID { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string Info { get; set; }
+
+        public UserTable(int userID, string eratosUserID, string email, string name, string auth0ID, DateTime createdAt, string info)
+        {
+            UserID = userID;
+            EratosUserID = eratosUserID;
+            Email = email;
+            Name = name;
+            Auth0ID = auth0ID;
+            CreatedAt = createdAt;
+            Info = info;
+        }
+    }
+}
